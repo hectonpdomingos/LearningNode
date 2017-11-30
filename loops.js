03fa5a4422fe5409f847172054dce015ec9c4754
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-30 18:16:04 -0200

    Loops

---
+++ loops.js
@@ -0,0 +1,13 @@
+var number = 0
+console.log("Executing do while loop")
+do {
+    number++;
+    console.log("Number: " + number);
+
+} while (number < 10)
+
+
+console.log("Executing for loop")
+for (var i = 0; i < 10; i++) {
+    console.log("For loop in: " + i)
+}
