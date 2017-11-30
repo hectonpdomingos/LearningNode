9b32ecdb83f67d2411077a6f04b5ececc39459e1
Hecton Domingos <hectonpdomingos@gmail.com>
2017-11-30 18:30:15 -0200

    conditionals

---
+++ conditionals.js
@@ -0,0 +1,21 @@
+var hotel = 89.99
+var gas = 100
+var rent = 700
+
+
+var total = hotel + gas + rent
+
+console.log("The total is: " + total)
+
+
+if (total > 700) {
+    console.log("We need save more money! ")
+}
+if (total == 400) {
+    console.log("The exatcly amount of money we have! ")
+}
+if (total < 400) {
+    console.log("Great!! ")
+} else if (total == 889.99) {
+    console.log("My limit!")
+}
