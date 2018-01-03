6712cf1008bd0045976ba51dfc24b01e91696b74
Hecton Domingos <hectonpdomingos@gmail.com>
2018-01-03 11:23:41 -0200

    route options

--- srv2-file-html.js
+++ srv2-file-html.js
@@ -2,11 +2,34 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+//static dir
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (request, Response) {
+    Response.sendFile(path.join(__dirname + '/index.html'));
 });
-app.get('/readme/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/readme.html'));
+
+
+app.get('/about', (request, Response) => {
+    Response.send('About page');
+});
+
+app.get('/readme', (request, Response) => {
+    Response.sendFile(path.join(__dirname + '/readme.html'));
+});
+
+//Response json obj
+app.get('/json', (request, Response) => {
+    Response.send({
+        company: 'Hecton Support',
+        country: 'Brazil',
+        owner: 'Hecton Domingos',
+        services: [
+            'Network Security',
+            'Kotlin Development',
+            'Linux server administration'
+        ]
+    });
 });
 
 app.listen(5000);
